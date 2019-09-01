846a50e7ab3605c5f0fb1b110eb72723e5fa49bb
cmalbuquerque <carolinamalbuquerque29@gmail.com>
2019-09-01 02:09:37 +0100

    Education

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
  $(function() {
+$(document).scroll(function () {
+		var $nav = $(".navbar-fixed-top");
+		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+ 	
     $('.scroll-down').click (function() {
       $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
       return false;
