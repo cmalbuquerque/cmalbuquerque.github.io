94a8b18157f6dddf4998e6f4480ff1e79008e8b3
cmalbuquerque <carolinamalbuquerque29@gmail.com>
2019-09-02 23:38:00 +0100

    Animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- $(function() {
+$(function() {
 $(document).scroll(function () {
 		var $nav = $(".navbar-fixed-top");
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
@@ -9,4 +9,11 @@ $(document).scroll(function () {
       return false;
     });
   });
-  
+
+$(document).ready(function () {
+    AOS.init({
+        duration: 1200,
+        once: false,
+        disable: 'mobile'
+    });
+});
