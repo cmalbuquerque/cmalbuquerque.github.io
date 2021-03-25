fa746c1ec105c25857052ac4e1df3051b9d63d4a
Carolina Albuquerque <carolinamalbuquerque29@gmail.com>
2021-03-25 19:49:07 +0000

    Home + about page

--- js/script.js
+++ js/script.js
@@ -1,82 +1,91 @@
 $(function() {
-  $(document).scroll(function () {
-      var $nav = $(".navbar-fixed-top");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $(document).scroll(function() {
+        var $nav = $(".sidenav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.width());
     });
-    
-      $('.scroll-down').click (function() {
-        $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
-        return false;
-      });
+});
+
+$(document).ready(function() {
+    AOS.init({
+        duration: 1200,
+        once: false
     });
 
-  $(document).ready(function () {
-      AOS.init({
-          duration: 1200,
-          once: false
-      });
-    
-  /* Preloader */
-	$(window).on('load', function() {
-		var preloaderFadeOutTime = 500;
-		function hidePreloader() {
-			var preloader = $('.spinner-wrapper');
-			setTimeout(function() {
-        preloader.fadeOut(preloaderFadeOutTime);
-        consoleText(['Hello.', 'I am Carolina Albuquerque' ], 'text',['skygrey','skygrey']);
-      }, 500);
-      
-		}
-    hidePreloader();
-    
-  });
+    /* Preloader */
+    $(window).on('load', function() {
+        var preloaderFadeOutTime = 500;
 
-  function consoleText(words, id, colors) {
-    if (colors === undefined) colors = ['#fff'];
-    var visible = true;
-    var con = document.getElementById('console');
-    var letterCount = 1;
-    var x = 1;
-    var waiting = false;
-    var target = document.getElementById(id)
-    target.setAttribute('style', 'color:' + colors[0])
-    window.setInterval(function() {
-  
-      if (letterCount === 0 && waiting === false) {
-        waiting = true;
-        target.innerHTML = words[0].substring(0, letterCount)
-        window.setTimeout(function() {
-          var usedColor = colors.shift();
-          colors.push(usedColor);
-          var usedWord = words.shift();
-          words.push(usedWord);
-          x = 1;
-          target.setAttribute('style', 'color:' + colors[0])
-          letterCount += x;
-          waiting = false;
-        }, 1000)
-      } else if (letterCount === words[0].length + 1 && waiting === false) {
-        waiting = true;
-        window.setTimeout(function() {
-          x = -1;
-          letterCount += x;
-          waiting = false;
-        }, 1000)
-      } else if (waiting === false) {
-        target.innerHTML = words[0].substring(0, letterCount)
-        letterCount += x;
-      }
-    }, 100)
-    window.setInterval(function() {
-      if (visible === true) {
-        con.className = 'console-underscore hidden'
-        visible = false;
-  
-      } else {
-        con.className = 'console-underscore'
-  
-        visible = true;
-      }
-    }, 400)
-  }
+        function hidePreloader() {
+            var preloader = $('.spinner-wrapper');
+            setTimeout(function() {
+                preloader.fadeOut(preloaderFadeOutTime);
+                consoleText(['Hello.', 'I am Carolina Albuquerque'], 'text', ['skygrey', 'skygrey']);
+            }, 500);
+
+        }
+        hidePreloader();
+
+    });
+
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+    function consoleText(words, id, colors) {
+        if (colors === undefined) colors = ['#fff'];
+        var visible = true;
+        var con = document.getElementById('console');
+        var letterCount = 1;
+        var x = 1;
+        var waiting = false;
+        var target = document.getElementById(id)
+        target.setAttribute('style', 'color:' + colors[0])
+        window.setInterval(function() {
+
+            if (letterCount === 0 && waiting === false) {
+                waiting = true;
+                target.innerHTML = words[0].substring(0, letterCount)
+                window.setTimeout(function() {
+                    var usedColor = colors.shift();
+                    colors.push(usedColor);
+                    var usedWord = words.shift();
+                    words.push(usedWord);
+                    x = 1;
+                    target.setAttribute('style', 'color:' + colors[0])
+                    letterCount += x;
+                    waiting = false;
+                }, 1000)
+            } else if (letterCount === words[0].length + 1 && waiting === false) {
+                waiting = true;
+                window.setTimeout(function() {
+                    x = -1;
+                    letterCount += x;
+                    waiting = false;
+                }, 1000)
+            } else if (waiting === false) {
+                target.innerHTML = words[0].substring(0, letterCount)
+                letterCount += x;
+            }
+        }, 100)
+        window.setInterval(function() {
+            if (visible === true) {
+                con.className = 'console-underscore hidden'
+                visible = false;
+
+            } else {
+                con.className = 'console-underscore'
+
+                visible = true;
+            }
+        }, 400)
+    }
 });
